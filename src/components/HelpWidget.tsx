89a703a8e0890844a0ac7beb6d2445c35b7354b0
robedgar <116982166+robedgar@users.noreply.github.com>
2022-12-17 20:56:59 +0000

    initial message panel

--- src/components/HelpWidget.tsx
+++ src/components/HelpWidget.tsx
@@ -1,7 +1,35 @@
 import { useState } from "react";
 
+type TMessage = {
+  message: string;
+  id: string;
+  sender: string;
+};
+
 export const HelpWidget = () => {
   const [isChatPanelDisplayed, setIsChatPanelDisplayed] = useState(false);
+  const [senderId, setSenderId] = useState("0");
+
+  const [messages, setMessages] = useState<TMessage[]>([
+    {
+      message: "Hello, how can we help you today?",
+      id: "bfgrgffgf44",
+      sender: "1",
+    },
+    { message: "I need help fixing my computer", id: "sjwwee67h", sender: "0" },
+    {
+      message: "Hello, how can we help you today?",
+      id: "bfgrgffgf44",
+      sender: "1",
+    },
+    { message: "I need help fixing my computer", id: "sjwwee67h", sender: "0" },
+    {
+      message: "Hello, how can we help you today?",
+      id: "bfgrgffgf44",
+      sender: "1",
+    },
+    { message: "I need help fixing my computer", id: "sjwwee67h", sender: "0" },
+  ]);
 
   return isChatPanelDisplayed ? (
     <div
@@ -10,15 +38,23 @@ export const HelpWidget = () => {
     flex h-96 w-72 flex-col justify-between bg-white p-6"
     >
       <button
-        className="absolute top-4 right-4 hover:text-red-400"
+        className="absolute top-2 right-2 hover:text-red-400"
         onClick={() => setIsChatPanelDisplayed(false)}
       >
         X
       </button>
 
       <ul>
-        <li>Hello, how can we help you today?</li>
-        <li>I need help fixing my computer</li>
+        {messages.map(({ message, id, sender }) => (
+          <li
+            className={` mb-2 rounded p-1 ${
+              sender === senderId ? "bg-gray-50" : "bg-blue-200"
+            }`}
+            key={id}
+          >
+            {message}
+          </li>
+        ))}
       </ul>
 
       <form className="flex">
